14f25d9893f61b86d8ce30576dea969b8024f38c
mithunprem99 <mithunprem1234@gmail.com>
2023-06-01 14:46:07 +0530

    GREEN:AttributeError: module 'game_of_life' has no attribute 'get_neighbors' fixed

--- game_of_life.py
+++ game_of_life.py
@@ -14,6 +14,16 @@ def initialize_grid(size, prob):
         grid.append(row)
     return grid
 
+def get_neighbors(board, row, col):
+    rows = len(board)
+    cols = len(board[0])
+    live_neighbors = 0
+    for i in range(max(0, row-1), min(rows, row+2)):
+        for j in range(max(0, col-1), min(cols, col+2)):
+            if board[i][j] == 1 and (i != row or j != col):
+                live_neighbors += 1
+    return live_neighbors
+
 def calculate_next_state(cell, neighbors):
     num_alive = sum(neighbors)
     if cell == 1 and (num_alive == 2 or num_alive == 3):
